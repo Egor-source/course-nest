26fd58fc98d278d87d5114620604cc457d963410
Egor <ego_rk@mail.ru>
2023-11-01 23:54:59 +0400

    new: сделал первоночальную инициализацию клиента

---
+++ client/src/axios/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import store from '@/store'
+
+const axiosInstance = axios.create({
+  baseURL: `${process.env.SERVER_BASE_URL}/${process.env.GLOBAL_PREFIX ?? ''}`
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  const {accessToken} = store.getters['user/getUser']
+
+  config.headers.Authorization = `Bearer ${accessToken}`
+  config.headers['Content-Type'] = 'application/json'
+  return config
+})
+
+export default axiosInstance
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const webpack = require('webpack');
+const path = require('path')
+const env = require('dotenv')
+  .config({
+    path: path.join(__dirname, '..', '.env'),
+  }).parsed;
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin(Object.entries(env).reduce((acc, [key, value]) => {
+        acc[`process.env.${key}`] = JSON.stringify(value);
+        return acc
+      }, {}))
+    ]
+  }
 })
