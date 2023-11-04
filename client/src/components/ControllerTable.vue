66890fa3ff175c3f2e5812e6e6616f1b7c84a9c4
Egor <ego_rk@mail.ru>
2023-11-04 09:00:01 +0400

    new: добавил нормальный скролл в таблице объектов

--- client/src/components/ControllerTable.vue
+++ client/src/components/ControllerTable.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex-1">
+  <div class="flex-1 overflow-y-auto">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
+      <thead class="sticky top-0 text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
       <tr>
         <th
           class="px-6 py-3"
